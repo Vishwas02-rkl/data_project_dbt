569252ce4f8ab64be2033d285f2d3699dd6b989a
VishwasAggarwal804 <s3859098@student.rmit.edu.au>
2026-07-10 19:26:17 +1000

    Add DAG processor log symlink and update requirements for dbt packages
    - Created a symlink for the latest DAG processor log pointing to the date 2026-07-10.
    - Added dbt-core version 1.11.12 and dbt-databricks version 1.10.9 to requirements.txt.

---
+++ airflow/dags/orchestrate.py
@@ -0,0 +1,76 @@
+from airflow.sdk import dag, task
+from airflow.operators.bash import BashOperator
+
+
+@dag
+def orchestrate():
+
+    @task
+    def ingest_cdc():
+        return "CDC data ingested"
+    
+    @task.bash
+    def clean_target():
+        return """
+        set -e
+
+        cd /opt/airflow/walmart_project
+
+        rm -rf target
+        rm -rf logs
+        """
+
+    @task.bash
+    def source_freshness():
+        return """
+        set -e
+
+        cd /opt/airflow/walmart_project
+
+        rm -rf target
+
+        dbt source freshness \
+          --project-dir /opt/airflow/walmart_project \
+          --profiles-dir /opt/airflow/walmart_project
+        """
+    silver_technical = BashOperator(
+        task_id="silver_technical",
+        bash_command='cd /opt/airflow/walmart_project && dbt run --select silver_t',    
+    )
+    
+    silver_technical_test = BashOperator(
+        task_id="silver_technical",
+        bash_command='cd /opt/airflow/walmart_project && dbt test --select silver_t',    
+    )
+    
+    silver_business = BashOperator(
+        task_id="silver_business",
+        bash_command='cd /opt/airflow/walmart_project && dbt run --select silver_b',    
+    )
+    
+    silver_business_test = BashOperator(
+        task_id="silver_business_test",
+        bash_command='cd /opt/airflow/walmart_project && dbt test --select silver_b',    
+        
+    )
+    
+    gold_ephemeral = BashOperator(
+        task_id="gold_ephemeral",
+        bash_command='cd /opt/airflow/walmart_project && dbt run --select gold/ephemeral',    
+    )
+    
+    gold_dimensions = BashOperator(
+        task_id="gold_dimensions",
+        bash_command='cd /opt/airflow/walmart_project && dbt snapshot',    
+        
+    )
+    
+    gold_facts = BashOperator(
+        task_id="gold_facts",
+        bash_command='cd /opt/airflow/walmart_project && dbt run --select gold/fact',    
+    )
+
+    ingest_cdc()>> clean_target() >> source_freshness() >> silver_technical >> silver_technical_test >> silver_business >> silver_business_test >> gold_ephemeral >> gold_dimensions >> gold_facts
+
+
+orchestrate_dag = orchestrate()
